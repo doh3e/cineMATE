40f176cb6960f74f9ec83fb23acce3943511d030
hne9711 <hne9711@gmail.com>
2024-11-14 11:50:40 +0900

    community

--- back-end/community/views.py
+++ back-end/community/views.py
@@ -4,5 +4,107 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Review, Comment, Movieforyou
+from .serializers import ReviewSerializer, CommentSerializer, MovieforyouSerializer  
 
-# Create your views here.
+
+# Review 
+@api_view(['GET', 'POST'])
+def review_list(request):
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)  
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def review_detail(request, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Comment 
+@api_view(['GET', 'POST'])
+def comment_list(request, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+
+    if request.method == 'GET':
+        comments = review.review_comments.all()  
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user, review=review)  
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def comment_detail(request, review_pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk, review_id=review_pk)
+
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Movieforyou 
+@api_view(['GET', 'POST'])
+def movieforyou_list(request):
+    if request.method == 'GET':
+        movies = Movieforyou.objects.all()
+        serializer = MovieforyouSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MovieforyouSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user) 
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def movieforyou_detail(request, movie_pk):
+    movie = get_object_or_404(Movieforyou, pk=movie_pk)
+
+    if request.method == 'GET':
+        serializer = MovieforyouSerializer(movie)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = MovieforyouSerializer(movie, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
