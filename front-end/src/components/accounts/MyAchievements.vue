058f0ce9abf1e592f4473c9c0f5f662e959b2fde
Jieun Jung <wldms3333@gmail.com>
2024-11-27 00:37:54 +0900

    24.11.27 00:37 프로젝트 최종 완성

---
+++ front-end/src/components/accounts/MyAchievements.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>나의 업적</h3>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ front-end/src/components/accounts/MyBookmarks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bookmark-box">
+    {{ bookmark.title }} ({{ bookmark.release_date }})
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  bookmark: Object
+})
+
+</script>
+
+<style scoped>
+</style>
